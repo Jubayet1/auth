b7a31136951a1e2d595075e64afa22afbeb56ab3
Jubayet Hossain <jubayet0106@gmail.com>
2025-06-12 11:48:05 +0600

    files and routes setup

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/signup">Sign Up</a>
+            <a href="/login">Login</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Components/Home/Home.jsx'
+import Layout from './Components/LayOut/Layout.jsx'
+import SignUp from './Components/SignUp/SignUp.jsx'
+import Login from './Components/LogIn/Login.jsx'
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }, 
+      {
+        path: "/signup",
+        element: <SignUp />,
+      }, 
+      {
+        path: "login",
+        element: <Login />
+      }
+    ] 
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={route}>
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
